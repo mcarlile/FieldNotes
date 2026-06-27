002f02cef524b3a56a5f3a5c84746dc3d1a62ad5
McKenzie Carlile <mckenzie.carlile@gmail.com>
2026-06-27 10:46:51 -0700

    Add native SwiftUI app scaffold

---
+++ BigMiles/BigMiles/BigMilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigMilesApp.swift
+//  BigMiles
+//
+//  Created by McKenzie Carlile on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BigMilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
